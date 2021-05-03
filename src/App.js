5f87117ed112ae06e4d660cbb0e621506b5ed434
Sean <f4555xu87@gmail.com>
2021-05-03 21:10:51 +0800

    ready to set up firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Post from './Post';
+
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: 'kevin',
+      caption: 'wow it works',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' 
+    },
+    {
+      username: 'John',
+      caption: 'wow it works',
+      imageUrl: 'https://res.cloudinary.com/practicaldev/image/fetch/s--wCGgterD--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png' 
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      {
+        posts.map((post) => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase';
+import config from './config';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseApp = firebase.initializeApp(config);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
